2866d2d34000db4e3921212b02828651c520357a
unknown <kaushikyash262@gmail.com>
2023-08-25 04:27:53 +0530

    Completed Cryptocurrencies component and updated homepage

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.COINS_API_KEY
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com'
@@ -14,7 +14,7 @@ export const cryptoApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: () => createRequest('/coins')
+            query: (count) => createRequest(`/coins?limit=${count}`)
         })
     })
 })
